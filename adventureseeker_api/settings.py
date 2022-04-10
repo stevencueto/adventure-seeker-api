299a4cbf9329eb0659d957e6b65a717b729d7fe8
stevencueto <steven.cueto1305@gmail.com>
2022-04-10 01:18:00 -0400

    static

--- adventureseeker_api/settings.py
+++ adventureseeker_api/settings.py
@@ -20,10 +20,6 @@ load_dotenv()
 BDS_PWD = str(os.environ.get("DBS_PWD"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # edit this var
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -146,4 +142,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # add this
+)
+
