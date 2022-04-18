7377406dabf02e2e454918daae83e4a3deb9d79b
stevencueto <steven.cueto1305@gmail.com>
2022-04-18 02:59:17 -0400

    statics

--- adventureseeker_api/settings.py
+++ adventureseeker_api/settings.py
@@ -162,13 +162,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
-    
-    if not DEBUG:
-        STATIC_ROOT = ''
-    
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static/'),
-    ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # add this)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
